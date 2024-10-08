86489a606099cc156226e515cc80df984e947060
Rahul-AkaVector <rahulpathania160404@gmail.com>
2024-10-08 07:07:28 +0530

    Feature Engineering Completed on CPU,RAM,HDD,SSD

--- Machine_Learning/Laptop Price Predictor/laptop_data_cleaning.py
+++ Machine_Learning/Laptop Price Predictor/laptop_data_cleaning.py
@@ -71,6 +71,114 @@ df.drop(columns = ['ScreenResolution','width','height'], inplace=True)
 print(df.corr(numeric_only=True)['Price'])
 print(df.head())
 
+# ----------------------------------- CPU Name ----------------------------------
+
+df['Cpu Name'] = df['Cpu'].apply(lambda x:" ".join(x.split()[0:3]))
+print(df.head())
+print(df['Cpu Name'].value_counts())
+
+def fetch_processor(text):
+    if text == 'Intel Core i7' or text == 'Intel Core i5' or text == 'Intel Core i3' or text == 'Intel Celeron Dual':
+        return text
+    else:
+        if text.split()[0] == 'Intel' :
+            return 'Other Intel Processor'
+        else:
+            return 'AMD Processor'
+
+df['Cpu brand'] = df['Cpu Name'].apply(fetch_processor)
+# print(df.sample(20))
+df.drop(columns=['Cpu','Cpu Name'],inplace=True)
+print(df.sample(5))
+
+
+print(df['Memory'].sample(10))
+
+# ======================================== SSD ========================
+
+print(df['Memory'].head(24))
+
+# print(df['Memory'].str.split('SSD').head(24))
+
+df['test_ssd'] = df['Memory'].str.split('SSD')
+
+df['test_ssd'] = df['test_ssd'].apply(lambda x:  x[0] if len(x) == 2 else 0)
+
+
+df['test_ssd'] = df['test_ssd'].apply(lambda x:  x.split('GB')[0] if 'GB' in str(x) else x.split('TB')[0] if 'TB' in str(x) else 0)
+
+df['SSD'] = df['test_ssd'].astype('float32')
+df['SSD'] = df['SSD'].astype('int32')
+
+
+
+# =============================================== hdd ========================
+# print(df['Memory'].head(24))
+
+
+df['test_hdd'] = df['Memory'].str.split('+' or 'HDD')
+
+# print(df['test_hdd'].head(24))
+
+df['test_hdd'] = df['test_hdd'].apply(lambda x:  x[-1] if len(x) == 2 else  x[0]  if "HDD" in x[0] else 0)
+# print(df['test_hdd'].head(24))
+# print(df['test_hdd'].apply(lambda x:  x.split('GB')[0] if 'GB' in str(x) else x.split('TB')[0] if 'TB' in str(x) else 0 ).head(24))
+
+df['test_hdd'] = df['test_hdd'].apply(lambda x:  x.split('GB')[0] if 'GB' in str(x) else x.split('TB')[0] if 'TB' in str(x) else 0 )
+#
+# print(df['test_hdd'].sample(24))
+df['HDD'] = df['test_hdd'].astype('float32')
+df['HDD'] = df['HDD'].astype('int32')
+df['HDD'] = df['HDD'].apply(lambda x: x*1024 if x<10 else x)
+# print(df['HDD'].head(24))
+
+
+df.drop(columns=['test_ssd','test_hdd','Memory'],inplace=True)
+# print(df['Memory'].head(24))
+print(df.head(24))
+
+print(df.corr(numeric_only=True)['Price'])
+
+
+df['GPU brand'] = df['Gpu'].apply(lambda x:x.split()[0])
+print(df['GPU brand'].value_counts())
+
+df = df[df['GPU brand'] != 'ARM']
+
+df.drop(columns=['Gpu'],inplace=True)
+
+print(df.head())
+
+def cat_os(inp):
+    if inp == 'Windows 10' or inp == 'Windows 7' or inp=='Windows 10 S':
+        return 'Windows'
+    elif inp == 'macOS' or inp =='Mac OS X':
+        return 'Mac'
+    else:
+        return 'Others/No Os/Linux'
+
+df['os'] = df['OpSys'].apply(cat_os)
+df.drop(columns=['OpSys'],inplace=True)
+print(df.corr(numeric_only=True)['Price'])
+print(df.columns)
+print(df.head())
+
+
+# sns.displot(df['Price'])      # skewed distribution the price is not evenly distributed in the table which can lead to false values
+# plt.show()
+
+# If the distribution is skewed, it means that the graph is not symmetrical. It might have a long tail on one side, indicating that there are a few prices that are much higher than the rest. This can happen if there are some expensive items in the dataset that are significantly more costly than the majority of items.
+#
+# Why is this a problem? Skewed distributions can impact data analysis and modeling in a few ways:
+#
+# Misleading Interpretations: Skewed data can give a distorted view of the overall pattern or average price. For example, the mean (average) price can be heavily influenced by a few extremely high-priced items, making it appear higher than what most items actually cost.
+
+# sns.displot(np.log(df['Price']))
+# plt.show()
+
+
+
+
 
 
 
