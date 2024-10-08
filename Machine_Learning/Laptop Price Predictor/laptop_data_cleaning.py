fe314a8a409e052bb6c2cc268a5e37596158fb05
Rahul-AkaVector <rahulpathania160404@gmail.com>
2024-10-08 07:06:20 +0530

    Adjusting screen resolution column

--- Machine_Learning/Laptop Price Predictor/laptop_data_cleaning.py
+++ Machine_Learning/Laptop Price Predictor/laptop_data_cleaning.py
@@ -38,6 +38,39 @@ import matplotlib.pyplot as plt
 #
 # plt.show()
 
+# ---------------------------- Screen Resolution Adjuster -------------------
+
+print("\n",df['ScreenResolution'].value_counts)
+
+df['Touchscreen'] = df['ScreenResolution'].apply(lambda x :1 if 'Touchscreen' in x else 0)
+df['Ips'] = df['ScreenResolution'].apply(lambda x :1 if 'IPS' in x else 0)
+
+print(df.sample(5))
+
+
+new = df['ScreenResolution'].str.split('x',n=1,expand = True)
+df['new2'] = new[0]
+
+df['width'] = df['new2'].str.split().str[-1]
+df['height'] = new[1]
+
+df['width'] = df['width'].astype('int32')
+df['height'] = df['height'].astype('int32')
+
+df.drop(columns=['new2'],inplace=True)
+print(df.head())
+print(df.info())
+
+print(df.corr(numeric_only=True)['Price'])
+
+print()
+
+df['ppi'] =(df['width']**2 + df['height']**2)**0.5 / df['Inches']
+print(df.head())
+df.drop(columns = ['ScreenResolution','width','height'], inplace=True)
+print(df.corr(numeric_only=True)['Price'])
+print(df.head())
+
 
 
 
