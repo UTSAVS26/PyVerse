5ae4f6da68446b756eba559abba409fe55413db8
Rahul-AkaVector <rahulpathania160404@gmail.com>
2024-10-08 07:04:38 +0530

    Checking Data Columns and rows and other similarities

--- Machine_Learning/Laptop Price Predictor/laptop_data_cleaning.py
+++ Machine_Learning/Laptop Price Predictor/laptop_data_cleaning.py
@@ -1,6 +1,43 @@
 import numpy as np
 import pandas as pd
 
+df = pd.read_csv("laptop_data.csv")
+print(df.head())
+
+print(df.shape)             # to check the shape of the dataframe
+print(df.info( ))              # to check null values and datatype of a columns
+
+print(df.duplicated().sum())         # to check whether two rows are not duplicated
+
+print(df.isnull().sum())
+
+df.drop(columns=["Unnamed: 0"],inplace=True)
+print(df.head())
+
+df['Ram'] = df['Ram'].str.replace('GB','')
+df['Weight'] = df['Weight'].str.replace('kg','')
+
+df['Ram'] = df['Ram'].astype('int32')
+df['Weight'] = df['Weight'].astype('float32')
+
+print(df.head(5))
+print(df.info())
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# df['Company'].value_counts().plot(kind='bar')
+
+# sns.barplot(x= df['Company'] , y = df['Price'])
+# plt.xticks(rotation = 'vertical')
+
+# df['TypeName'].value_counts().plot(kind='bar')
+#
+# sns.barplot(x= df['TypeName'] , y = df['Price'])
+# plt.xticks(rotation = 'vertical')
+#
+# plt.show()
+
 
 
 
