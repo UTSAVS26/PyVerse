ea1dddfa7379ea9c866b01eae5630e1e69241053
Rahul-AkaVector <rahulpathania160404@gmail.com>
2024-10-09 07:20:04 +0530

    Online Video Editor Completed

--- Video Processing/Online Video Editor/edits.py
+++ Video Processing/Online Video Editor/edits.py
@@ -45,3 +45,94 @@ st.text("""🎥 Create stunning videos on the go with our Online Mini Video Edit
 clip = st.sidebar.file_uploader(" ", type=["mp4"])
 
 
+if clip:
+    with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as temp_file:
+        temp_filename = temp_file.name
+        temp_file.write(clip.read())
+
+    st.sidebar.markdown(
+        "<h2 style='text-align: center;'>Video Details 📝</h2>",
+        unsafe_allow_html=True)
+
+    clip = mp.VideoFileClip(temp_filename)
+    w, h = clip.size
+    st.sidebar.write(f"Video size 📏 : {w}x{h}")
+    duration = clip.duration
+    st.sidebar.write(f"Duration ⏱️: {duration.__round__()} seconds")
+    st.sidebar.write(f"Duration ⏱️: {(duration/60):.2f} minutes")
+    fps = clip.fps
+    st.sidebar.write(f"FPS🎞️: {fps.__round__()}")
+
+    st.sidebar.markdown(
+        "<h2 style='text-align: center;'>Crop ✂️ </h2>",
+        unsafe_allow_html=True)
+
+    start = st.sidebar.number_input("Start Time (in seconds) 🔺")
+    end = st.sidebar.number_input("End Time (in seconds) 🔻")
+
+    if end > start:
+        clip = clip.subclip(start, end)
+
+    st.sidebar.markdown(
+        "<h2 style='text-align: center;'>Video/Audio Fade 💨</h2>",
+        unsafe_allow_html=True)
+
+    fade_in = st.sidebar.number_input("Fade in seconds 🔺")
+    fade_out = st.sidebar.number_input("Fade out seconds 🔻")
+    duration = end - start
+
+    if fade_out < duration and fade_in < duration:
+        clip = clip.fx(vfx.fadein, fade_in)
+        clip = clip.fx(vfx.fadeout, fade_out)
+
+    st.sidebar.markdown(
+        "<h2 style='text-align: center;'>Resizing 🖼️</h2>",
+        unsafe_allow_html=True)
+
+    # Resizing
+    resize_width = st.sidebar.number_input("Resize Width 🔺", 1 , w , w)
+    resize_height = st.sidebar.number_input("Resize Height 🔻", 1, h ,h)
+    clip = clip.resize((resize_width, resize_height))
+
+    st.sidebar.markdown(
+        "<h2 style='text-align: center;'>Functions 🔩</h2>",
+        unsafe_allow_html=True)
+    # Mute checkbox
+    mute = st.sidebar.checkbox("Mute 🔇")
+    if mute:
+        clip = clip.fx(vfx.fadeout, duration)
+
+    # Flipping
+    flip_horizontal = st.sidebar.checkbox("Flip Horizontal  ↔️")
+    flip_vertical = st.sidebar.checkbox("Flip Vertical  ↕️")
+    if flip_horizontal:
+        clip = clip.fx(vfx.mirror_x)
+    if flip_vertical:
+        clip = clip.fx(vfx.mirror_y)
+
+    st.sidebar.markdown(
+        "<h2 style='text-align: center;'>Adjustments ⚙️</h2>",
+        unsafe_allow_html=True)
+    # Colour Change
+    color = st.sidebar.slider("Colour Change 🎨", 0.0, 2.0, 1.0)
+    clip = clip.fx(mp.vfx.colorx, color)
+
+    volume = st.sidebar.slider("Volume Change 🔊", 0.0, 4.0, 1.0)
+    clip = clip.volumex(volume)
+
+    # Save video to a temporary file
+    with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as temp_file:
+        temp_filename = temp_file.name
+        clip.write_videofile(temp_filename)
+
+    # Open the temporary file and read its contents as bytes
+    with open(temp_filename, "rb") as file:
+        video_bytes = file.read()
+
+    # Display the video in Streamlit
+    st.header("Edited Video 🔧")
+    st.subheader(" ")
+    st.video(video_bytes)
+    st.markdown(
+        "<h6 style='text-align: center;'>To download your edited video, click on the ⋯ (3 dots) at the bottom right corner ⬇️</h6>",
+        unsafe_allow_html=True)
