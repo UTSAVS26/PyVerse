d8e87328e8211db30d40cb1a1ffd6ed3440d7d7b
Sibangi Boxipatro <124905387+Sibangi-2911@users.noreply.github.com>
2024-10-13 15:49:33 +0530

    Update collect_data.py

--- Machine_Learning/Face Detection/collect_data.py
+++ Machine_Learning/Face Detection/collect_data.py
@@ -3,14 +3,15 @@ import cv2
 import urllib
 import numpy as np
 
-#This is the cascade classifier for the frontal face url with the ip address
+
 classifier = cv2.CascadeClassifier(r"C:\Users\Sibangi Boxipatro\Desktop\Face Detection\Face Detection\haarcascade_frontalface_default.xml")
 
 url = "http://192.168.43.1:8080/shot.jpg"
 
 
 data = []
-
+#Main loop
+#The loop continues until 100 faces are captured or the 'q' key is pressed
 while len(data) < 100:
     
     
@@ -20,7 +21,7 @@ while len(data) < 100:
     
     face_points = classifier.detectMultiScale(frame,1.3,5)
     
-    if len(face_points)>0:
+    if len(face_points)>0: #Capturing and processing image
         for x,y,w,h in face_points:
             face_frame = frame[y:y+h+1,x:x+w+1]
             cv2.imshow("Only face",face_frame)
@@ -28,19 +29,24 @@ while len(data) < 100:
                 print(len(data)+1,"/100")
                 data.append(face_frame)
                 break
-    cv2.putText(frame, str(len(data)),(100,100),cv2.FONT_HERSHEY_SIMPLEX,5,(0,0,255)) #code for the display of text that is the name after identification
-    cv2.imshow("frame",frame)
+    cv2.putText(frame, str(len(data)),(100,100),cv2.FONT_HERSHEY_SIMPLEX,5,(0,0,255)) 
+    cv2.imshow("frame",frame) #Showing frames and exiting
     if cv2.waitKey(30) == ord("q"):
         break
 cv2.destroyAllWindows()
-        
+
+#Saving captured data
 if len(data)== 100:
     name = input("Enter Face holder name : ")
     for i in range(100):
         cv2.imwrite("images/"+name+"_"+str(i)+".jpg",data[i])
     print("Done")
 else:
-    print("need more data")
+    print("need more data") #Handling insufficient data
+
+#This code assumes the URL provides a valid image stream.
+#The code might need adjustments depending on the expected face size for further processing.
+#Ensure you have OpenCV, NumPy, and the pre-trained classifier file for this code to work.
         
     
 
