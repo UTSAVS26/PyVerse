b2e5e25cc51a8ae5e9aacd306a71f7dcb99eb3d1
Sibangi Boxipatro <124905387+Sibangi-2911@users.noreply.github.com>
2024-10-13 15:32:05 +0530

    Update collect_data.py

--- Machine_Learning/Face Detection/collect_data.py
+++ Machine_Learning/Face Detection/collect_data.py
@@ -1,7 +1,9 @@
+#Import the necessary libraries
 import cv2
 import urllib
 import numpy as np
 
+#This is the cascade classifier for the frontal face url with the ip address
 classifier = cv2.CascadeClassifier(r"C:\Users\Sibangi Boxipatro\Desktop\Face Detection\Face Detection\haarcascade_frontalface_default.xml")
 
 url = "http://192.168.43.1:8080/shot.jpg"
@@ -26,7 +28,7 @@ while len(data) < 100:
                 print(len(data)+1,"/100")
                 data.append(face_frame)
                 break
-    cv2.putText(frame, str(len(data)),(100,100),cv2.FONT_HERSHEY_SIMPLEX,5,(0,0,255))
+    cv2.putText(frame, str(len(data)),(100,100),cv2.FONT_HERSHEY_SIMPLEX,5,(0,0,255)) #code for the display of text that is the name after identification
     cv2.imshow("frame",frame)
     if cv2.waitKey(30) == ord("q"):
         break
