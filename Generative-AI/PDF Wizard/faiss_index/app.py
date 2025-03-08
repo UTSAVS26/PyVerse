a68606c564cc747c922ef32710b087c8e697c923
tulu-g559 <garnab559@gmail.com>
2025-03-09 00:37:50 +0530

    Added PDF Wizard

---
+++ Generative-AI/PDF Wizard/faiss_index/app.py
@@ -0,0 +1,129 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+
+
+##Function that reads the pdd goes through each and every page
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+##Function that breaks text into chunks
+def get_text_chunks(text):
+    # Adjust chunk size and overlap as needed
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+
+
+##Function that saves the data we got from conversation to local(here), generally stored in data base
+#converting into chunks -> storing data in faiss vector
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    
+    # Ensure the directory exists
+    if not os.path.exists("faiss_index"):
+        os.makedirs("faiss_index")
+    
+    # Save the vector store index in the directory
+    vector_store.save_local("faiss_index")
+
+    
+
+
+#Function to give the prompt and ask the bot to act accordingly, giving the gemini model
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+
+
+
+##Function for user input, question
+#give the question-> do a similarity search on all the faiss vectors-> go with converstional chain-> response from chain
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    
+    # Check if the faiss_index file exists before loading
+    if not os.path.exists("faiss_index/index.faiss"):
+        st.error("FAISS index file not found. Please process the PDF files first.")
+        return
+    
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+    
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True
+    )
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
+
+
+##Main Function for streamlit app
+def main():
+    st.set_page_config("PDF Wizard")
+    st.header("Chat with multiple PDFs📄")
+
+    user_question = st.text_input("📎Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
