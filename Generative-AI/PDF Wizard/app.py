5e2827a4cb08b5ec0fe0a024350e3344258b51f2
tulu-g559 <garnab559@gmail.com>
2025-03-09 11:30:13 +0530

    Updated the latest version of gemini

--- Generative-AI/PDF Wizard/app.py
+++ Generative-AI/PDF Wizard/app.py
@@ -87,7 +87,7 @@ def get_conversational_chain():
 
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash",temperature=0.3)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
