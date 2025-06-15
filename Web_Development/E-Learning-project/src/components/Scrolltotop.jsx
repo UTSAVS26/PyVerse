5d9813d7cd9d902f2e088128c3b57cafebfcccde
Srigada Akshay Kumar <153109964+SrigadaAkshayKumar@users.noreply.github.com>
2025-06-15 10:46:51 +0530

    Update Scrolltotop.jsx

--- Web_Development/E-Learning-project/src/components/Scrolltotop.jsx
+++ Web_Development/E-Learning-project/src/components/Scrolltotop.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const Scrolltotop = () => {
+const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -11,4 +11,4 @@ const Scrolltotop = () => {
   return null;
 };
 
-export default Scrolltotop;
+export default ScrollToTop;
