d3ba468369329e7030691be19c588e84f25adb3c
Abhishek Sharma <165680680+AbhishekSharma-9@users.noreply.github.com>
2025-07-02 22:27:33 +0530

    Add files via upload

---
+++ Advanced_Projects/Resume Scanner/extractor.py
@@ -0,0 +1,9 @@
+
+from PyPDF2 import PdfReader
+
+def extract_text_from_pdf(file_path):
+    reader = PdfReader(file_path)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
