04a2819eb082f399bdd84c30190f1d1f8ad199da
Abhishek-Verma0 <neeleshabhishek@gmail.com>
2025-07-27 22:18:19 +0530

    Changes: Done as asked in previous PR#1501 that I closed

--- Cybersecurity_Tools/IP-Changer/ip-changer.sh
+++ Cybersecurity_Tools/IP-Changer/ip-changer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-[[ "$UID" -ne 0 ]] && {
-    echo "Script must be run as root."
+# insuring script executed as root
+[[ "$UID" -ne 0 ]] && { 
+    echo "Script must run as root. use sudo "
     exit 1
 }
 
-install_packages() {
+install_pacakage_according_to_OS() {
     local distro
     distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
     distro=${distro//\"/}
@@ -22,7 +22,8 @@ install_packages() {
         *"Arch"*)
             pacman -S --noconfirm curl tor
             ;;
-        *)
+        *) 
+        # give warning if not in above distro to install torand curl manually
             echo "Unsupported distribution: $distro. Please install curl and tor manually."
             exit 1
             ;;
@@ -31,14 +32,14 @@ install_packages() {
 
 if ! command -v curl &> /dev/null || ! command -v tor &> /dev/null; then
     echo "Installing curl and tor"
-    install_packages
+    install_pacakage_according_to_OS
 fi
 
 if ! systemctl --quiet is-active tor.service; then
     echo "Starting tor service"
     systemctl start tor.service
 fi
-
+ # this gets current Ip
 get_ip() {
     local url get_ip ip
     url="https://checkip.amazonaws.com"
@@ -46,7 +47,7 @@ get_ip() {
     ip=$(echo "$get_ip" | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
     echo "$ip"
 }
-
+# this function changes ip by reloading tor services
 change_ip() {
     echo "Reloading tor service"
     systemctl reload tor.service
@@ -62,11 +63,12 @@ cat << EOF
     | ||  __/_____| |___|  _  |/ ___ \| |\  | |_| | |___|  _ < 
    |___|_|         \____|_| |_/_/   \_\_| \_|\____|_____|_| \_\
 EOF
-
+# main modifcation done below
 while true; do
     read -rp $'\033[34mEnter time interval in seconds (type 0 for irandom interval): \033[0m' interval
     read -rp $'\033[34mEnter number of times to change IP address (type 0 for infinite IP changes): \033[0m' times
-
+    # now if user give 0 for interval a random interval assigned
+    # and if 0 for how many changes then infinite changes
     if [ "$interval" -eq "0" ] || [ "$times" -eq "0" ]; then
         echo "Starting infinite IP changes"
         while true; do
