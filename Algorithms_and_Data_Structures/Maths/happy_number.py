35cd779abf717385ac104215e593d4a5695a520a
Srijani Das <srijani0107@gmail.com>
2025-09-20 15:56:24 +0530

    Update happy_number.py

--- Algorithms_and_Data_Structures/Maths/happy_number.py
+++ Algorithms_and_Data_Structures/Maths/happy_number.py
@@ -2,8 +2,8 @@ def is_happy(n):
     seen = set()
     while n != 1 and n not in seen:
         seen.add(n)
-        n = sum(int(d)**2 for d in str(n))
-    return n == 1
+        n = sum(int(d)**2 for d in str(n)) 
+    return n == 1   # if the number eventually becomes 1 by repeatedly summing the squares of its digits return true.
 
 n = int(input("Enter a number: "))
 print("Happy Number" if is_happy(n) else "Not a Happy Number")
